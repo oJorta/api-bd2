852e4d3f9ba1c188e14b30458a4e09db304b0a07
Joao Pedro <jpcsnoberto@gmail.com>
2024-04-24 00:55:23 -0300

    fix: port

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ const express = require('express')
 const mysql = require('mysql');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Configuração do banco de dados
 const db = mysql.createConnection({
