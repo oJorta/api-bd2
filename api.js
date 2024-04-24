ca55969d0acce51497477e20ac247464a5adcd2f
Joao Pedro <jpcsnoberto@gmail.com>
2024-04-24 00:30:32 -0300

    feat: basic api structure

---
+++ api.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+// Configuração do banco de dados
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'academico'
+});
+
+// Conectar ao banco de dados
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Conectado ao banco de dados MySQL');
+});
+
+// Rota para executar consulta
+app.get('/consulta', (req, res) => {
+  const query = 'SELECT * FROM aluno';
+  
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).send('Erro ao executar a consulta');
+      throw err;
+    }
+    res.json(result);
+  });
+});
+
+// Iniciar servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
